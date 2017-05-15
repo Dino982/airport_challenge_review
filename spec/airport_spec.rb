22c12ba417ad4931c9f29d1f1a7fc31ccfbef504
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-15 22:06:09 +0100

    User story 2 complete

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,14 @@
 require 'airport'
 
-subject(:airport) { described_class.new }
 describe Airport do
+
+  subject(:airport) {described_class.new}
+
   it 'instructs a plane to land' do
-    expect(subject).to respond_to(:land).with(1).argument
+    expect(airport).to respond_to(:land).with(1).argument
+  end
+
+  it 'instructs a plane to take_off' do
+    expect(airport).to respond_to(:take_off).with(1).argument
   end
 end

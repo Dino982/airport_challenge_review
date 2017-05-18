7bb71ad9b81c68d9cbd50be92290f01b56838df1
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-18 21:09:20 +0100

    US 4 (WIP) - Half of User story 4

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,13 +5,13 @@ describe 'user stories' do
   it 'so planes land at airports, instruct a plane to land' do
     airport = Airport.new(20)
     plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return false
     expect { airport.land(plane) }.not_to raise_error
   end
 
   # As an air traffic controller
   # So I can get passengers on the way to their destination
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
-
   it 'so planes take off from airports, instructs a plane to take off' do
   airport = Airport.new(20)
   plane = Plane.new
@@ -21,12 +21,28 @@ describe 'user stories' do
   # As an air traffic controller
   # So I can avoid collisions
   # I want to prevent planes from landing when airport full
-  it 'does not allow planes to land when airport full' do
-    airport = Airport.new(20)
-    plane = Plane.new
-      20.times do
-        airport.land(plane)
-      end
-      expect { airport.land(plane)}.to raise_error 'Cant land plane: airport full'
+  context 'when airport full' do
+    it 'does not allow planes to land' do
+      airport = Airport.new(20)
+      plane = Plane.new
+      allow(airport).to receive(:stormy?).and_return false
+        20.times do
+          airport.land(plane)
+        end
+        expect { airport.land(plane)}.to raise_error 'Cant land plane: airport full'
+    end
+  end
+
+
+  # As an air traffic controller
+  # So I can avoid accidents
+  # I want to prevent planes from landing or  taking off during stormy weather
+  context 'when weather is stormy' do
+    it 'does not allow planes to land' do
+      airport = Airport.new(20)
+      plane = Plane.new
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land(plane)}.to raise_error 'Cannot land plane: weather is stormy'
+    end
   end
 end
